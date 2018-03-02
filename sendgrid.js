ec108998a67c6612382b48c69072fdf74b16dc6c
Giang Nguyen <luckytintinnz@yahoo.com>
2018-03-02 16:07:48 +1300

    Add sendgrid template

--- sendgrid.js
+++ sendgrid.js
@@ -1,13 +1,22 @@
 const sgMail = require('@sendgrid/mail');
 
+const TEMPLATE_ID = '15240169-74cf-4c8a-8f12-42014105341a';
+
 const sendEmail = () => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  sgMail.setSubstitutionWrappers('{{', '}}');
+
   const msg = {
     to: 'g.nguyen+msg@crimsoneducation.org',
     from: 'g.nguyen+from@crimsoneducation.org',
     subject: 'Sending with SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    html: '<strong>Hello World</strong>',
+    templateId: TEMPLATE_ID,
+    substitutions: {
+      heading0: 'John Smith',
+      paragraph0: 'Say hello to him.',
+    },
   };
   sgMail.send(msg);
 };
